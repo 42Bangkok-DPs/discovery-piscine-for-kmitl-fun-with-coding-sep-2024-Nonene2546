6217a6e22c716719d666c6324ca60e52a2764510
Chaksawat <88503339+Nonene2546@users.noreply.github.com>
2024-09-13 00:37:23 +0700

    fix todo js

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -4,30 +4,28 @@ $(document).ready(function () {
   function saveTodo() {
     const todos = [];
     const links = $("#ft_list").find("a").toArray();
-    console.log(links);
     links.forEach((link) => {
       const todo = {
         id: link.id,
-        text: $(link).find(".todo").text(),
+        text: encodeURIComponent($(link).find(".todo").text()),
       };
       todos.push(todo);
     });
-    document.cookie = `todos=${JSON.stringify(todos)}; path=/;`;
+    const encode_todos = encodeURIComponent(JSON.stringify(todos));
+    document.cookie = encode_todos; 
   }
 
   function loadTodo() {
-    const cookieString = document.cookie
-      .split("; ")
-      .find((row) => row.startsWith("todos="));
+    const cookieString = document.cookie;
     if (cookieString) {
-      const todos = JSON.parse(cookieString.split("=")[1]);
+      const todos = JSON.parse(decodeURIComponent(cookieString));
       todos.forEach((todo) => {
         $("<a></a>")
           .attr("id", todo.id)
           .click(function () {
             deleteTodo(this.id);
           })
-          .append($("<div></div>").addClass("todo").text(todo.text))
+          .append($("<div></div>").addClass("todo").text(decodeURIComponent(todo.text)))
           .appendTo($("#ft_list"));
       });
       todoId = todos.length
@@ -55,7 +53,6 @@ $(document).ready(function () {
     let deleteMsg = confirm("Are you sure you want to delete this todo?");
     if (deleteMsg) {
       $("#" + id).remove();
-    //   alert("To Do delete successfully!");
       saveTodo();
     }
   }
